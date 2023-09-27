f36947b27ecb52f7657feddc96c6d452b82e0797
JoeBlanc <duong20702@gmail.com>
2023-09-27 13:59:41 +0700

    ultiliza functions: theme control

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -3,6 +3,7 @@ import { Header, AddBook, Table, Theme } from './components';
 import { useState, useEffect } from 'react';
 
 function App({data}) {
+  // book list local storage
   const [bookList, setBookList] = useState(()=>{
     const localData = localStorage.getItem('bookList');
     return localData ? JSON.parse(localData) : data;
@@ -11,19 +12,19 @@ function App({data}) {
     localStorage.setItem('bookList', JSON.stringify(bookList));
   }, [bookList]);
 
+  // theme local storage
+  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
   const [currentTheme, setCurrentTheme] = useState(() => {
     const localTheme = localStorage.getItem('theme');
-    return localTheme ? localTheme : null;
+    return localTheme ? localTheme : systemTheme;
   });
   useEffect(() => {
-    themeCheck()
+    localStorage.setItem('theme', JSON.stringify(currentTheme));
   }, [currentTheme]);
 
-  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-
-  // const userTheme = localStorage.getItem('theme');
+  // initial theme check
+  useEffect(() => {themeCheck()})
   function themeCheck() {
-    console.log('themeCheck');
     if (currentTheme === 'dark' || (!currentTheme && systemTheme ==='dark')) {
       document.documentElement.classList.add('dark');
       setCurrentTheme('dark');
@@ -31,29 +32,30 @@ function App({data}) {
       document.documentElement.classList.add('light');
       setCurrentTheme('light');
     }
-    localStorage.setItem('theme', JSON.stringify(currentTheme));
   }
-
+  // switch theme
   function switchTheme() {
     if(document.documentElement.classList.contains('dark')) {
       document.documentElement.classList.remove('dark');
       document.documentElement.classList.add('light');
-      localStorage.setItem('theme', 'light');
+      // localStorage.setItem('theme', 'light');
+      setCurrentTheme('light');
       return
     } else {
       document.documentElement.classList.remove('light');
       document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
+      // localStorage.setItem('theme', 'dark');
+      setCurrentTheme('dark');
       return
     }
   } 
-
+  // add book
   function handleAddBook(name, author, topic) {
     setBookList(currentBookList => {
       return [{name, author, topic} ,...currentBookList ]
     });
   }
-
+  // delete book
   function handleDeleteBook(name) {
     let confirm = window.confirm('Are you sure you want to delete this book?');
     if (confirm) {
